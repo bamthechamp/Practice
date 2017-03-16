4eec84f6e7cb52d0ff7dc3a6e4e4bcd7ee8fb0fd
brandon mitchell <B-Mitch@brandons-MacBook-Pro.local>
2017-03-16 12:27:18 -0500

    Initial Commit

---
+++ Practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Practice
+//
+//  Created by brandon mitchell on 3/16/17.
+//  Copyright © 2017 brandon mitchell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
